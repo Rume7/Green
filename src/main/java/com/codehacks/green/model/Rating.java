2ee536da497493da5caeb6c440f4d7acd4ea061c
Rume7 <electengrhume@gmail.com>
2022-10-26 05:44:45 +0100

    Created the Rating class

--- src/main/java/com/codehacks/green/model/Rating.java
+++ src/main/java/com/codehacks/green/model/Rating.java
@@ -1,2 +1,25 @@
-package com.codehacks.green.model;public class Rating {
+package com.codehacks.green.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Entity
+@Table(name="Post_Rating")
+@Data
+public class Rating {
+
+    @Id
+    @GeneratedValue
+    private UUID Id;
+    private Float rate;
+    private LocalDateTime ratingDate;
+    private User user;
 }
