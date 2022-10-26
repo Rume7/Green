67bf628ed16c907f7239b50f48a849a5f8f9a56c
Rume7 <electengrhume@gmail.com>
2022-10-26 05:45:10 +0100

    Created the comment class.

--- src/main/java/com/codehacks/green/model/Comment.java
+++ src/main/java/com/codehacks/green/model/Comment.java
@@ -1,2 +1,25 @@
-package com.codehacks.green.model;public class Comment {
+package com.codehacks.green.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Entity
+@Table(name = "Comment")
+@Data
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    private UUID Id;
+
+    @OneToOne
+    private User user;
+    private String comment;
+    private LocalDateTime commentDate;
+
 }
