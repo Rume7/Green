979fa09478a89f51242c044a589466ed0f216425
Rume7 <electengrhume@gmail.com>
2022-10-26 05:44:23 +0100

    Created the Post class.

--- src/main/java/com/codehacks/green/model/BlogPost.java
+++ src/main/java/com/codehacks/green/model/BlogPost.java
@@ -1,2 +1,39 @@
-package com.codehacks.green.model;public class BlogPost {
+package com.codehacks.green.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Entity
+@Table(name = "BlogPost")
+@Data
+public class BlogPost {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String title;
+
+    private String short_description;
+
+    private String description;
+
+    private LocalDateTime publishedDate;
+
+    @OneToOne
+    private Author author;
+
+    @OneToMany
+    private List<Comment> commentsList;
+
+    @OneToMany
+    private double postRating;
+
+
 }
