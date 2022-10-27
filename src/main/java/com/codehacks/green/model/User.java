3791c04e8c06afd976287a67115b72729b85a591
Rume7 <electengrhume@gmail.com>
2022-10-27 18:30:43 +0100

    Added the User class.

--- src/main/java/com/codehacks/green/model/User.java
+++ src/main/java/com/codehacks/green/model/User.java
@@ -1,2 +1,45 @@
-package com.codehacks.green.model;public class User {
+package com.codehacks.green.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Entity
+@Table(name="Subscriber")
+@Data
+public class User {
+
+    @Id
+    private UUID Id;
+
+    @NotBlank
+    private String username;
+    private String firstName;
+    private String lastName;
+
+    @NotBlank
+    private String emailAddress;
+    private LocalDateTime dateCreated;
+    private LocalDateTime lastUpdated;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(Id, user.Id) && Objects.equals(emailAddress, user.emailAddress);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id, emailAddress);
+    }
 }
